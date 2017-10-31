37370e32b22988f197a29d62dfc38929c3ab72e9
Nick Ye <nick.ye888@gmail.com>
2017-10-31 22:07:07 +1300

    Added tests to test decrease of SellIn and Quality

---
+++ Tests/GildedRoseShould.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using csharp.Logic;
+using NUnit.Framework;
+
+namespace csharp.Tests
+{
+    [TestFixture]
+    public class GildedRoseShould
+    {
+        [Test]
+        public void DecreaseTheSellInOfAnItemByOne()
+        {
+            IList<Item> items = new List<Item> { new Item { Name = "foo", SellIn = 1, Quality = 1 } };
+            var qualityUpdater = new GildedRose(items);
+            var expectedItems = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            qualityUpdater.UpdateQuality();
+            Assert.AreEqual(expectedItems[0].SellIn, items[0].SellIn);
+            
+        }
+
+        [Test]
+        public void DecreaseTheQualityOfAnItemByOne()
+        {
+            IList<Item> items = new List<Item> { new Item { Name = "foo", SellIn = 1, Quality = 1 } };
+            var qualityUpdater = new GildedRose(items);
+            var expectedItems = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            qualityUpdater.UpdateQuality();
+            Assert.AreEqual(expectedItems[0].Quality, items[0].Quality);
+
+        }
+    }
+}
