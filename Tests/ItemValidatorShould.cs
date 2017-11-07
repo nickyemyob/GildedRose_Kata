efc79e62c3289435d57f8e407ddbed69f37201c5
Nick Ye <nick.ye888@gmail.com>
2017-11-08 00:29:22 +1300

    Added validator class and test for checking for aged brie

---
+++ Tests/ItemValidatorShould.cs
@@ -0,0 +1,25 @@
+﻿using csharp.Logic;
+using csharp.Validators;
+using NUnit.Framework;
+
+namespace csharp.Tests
+{
+    [TestFixture]
+    class ItemValidatorShould
+    {
+        [Test]
+        public void CheckIfItemIsAgedBrie()
+        {
+            var item = new Item()
+            {
+                Name = "Aged Brie"
+            };
+
+            var validator = new ItemValidator();
+            var expected = validator.IsAgedBrie(item);
+
+            Assert.IsTrue(expected);
+
+        }
+    }
+}
---
+++ Validators/ItemValidator.cs
@@ -0,0 +1,12 @@
+﻿using csharp.Logic;
+
+namespace csharp.Validators
+{
+    internal class ItemValidator
+    {
+        public bool IsAgedBrie(Item item)
+        {
+            return item.Name == "Aged Brie";
+        }
+    }
+}
